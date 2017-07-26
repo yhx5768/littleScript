ef4ebb2ce0ff0722c87ba4ef5af206c2517b0704
Soar <yhx5768@hotmail.com>
2017-07-26 22:39:31 +0800

    md5 test failed

--- forThePhx.py
+++ forThePhx.py
@@ -1,8 +1,31 @@
 import urllib2
+import hashlib
+import time
 
-request = urllib2.Request('http://www.soarx.cc/index.html')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-firstdatastream.add_header('If-Modified-Since',
-            firstdatastream.headers.get('Last-Modified'))
-seconddatasteam = opener.open(request)
+url = 'https://bandwagonhost.com/cart.php'
+test_url = 'http://www.soarx.cc/'
+headers = {
+            'authority' : 'bandwagonhost.com',
+           'path' : '/cart.php',
+           'referer' : 'https://bandwagonhost.com/clientarea.php',
+           'user-agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36'}
+
+request = urllib2.Request(test_url)
+for header in headers:
+    request.add_header(header, headers[header])
+response = urllib2.urlopen(request)
+# print response.headers.dict
+page = response.read()
+hash_page = hashlib.md5(page)
+
+time.sleep(60)
+
+second_request = urllib2.Request(test_url)
+second_response = urllib2.urlopen(second_request)
+second_page = second_response.read()
+hash_page2 = hashlib.md5(second_page)
+
+if (hash_page.hexdigest() == hash_page2.hexdigest()):
+    print 'no change'
+else:
+    print 'changed'
